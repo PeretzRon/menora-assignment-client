c50a96d938f47973cfb89ba8230352adc4ecd896
ronpe <ron.peretz1@gmail.com>
2021-08-13 14:54:26 +0300

    implementation of header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className={classes.Header}>Menora - Movies IMDB</h2>
+        </div>
+    );
+}
+
+export default Header;
